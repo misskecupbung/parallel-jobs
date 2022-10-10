26c23cd892441e9f6b90b58dcb3d92c699af3674
Ananda <ananda.dwirahmawati313@gmail.com>
2022-10-10 22:53:11 +0700

    edit hello-py

--- hello-py/server.py
+++ hello-py/server.py
@@ -4,17 +4,9 @@ import time
 from flask import Flask
 app = Flask(__name__)
 
-START = time.time()
-
-def elapsed():
-    running = time.time() - START
-    minutes, seconds = divmod(running, 60)
-    hours, minutes = divmod(minutes, 60)
-    return "%d:%02d:%02d" % (hours, minutes, seconds)
-
 @app.route('/')
 def root():
-    return "Hello World (Python)! (up %s)\n" % elapsed()
+    return "Hello World (Python)!"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
